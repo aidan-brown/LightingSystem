c758879a2f8b4dbb6604e9508a480ff525a49288
Aidan Brown (RIT Student) <afb3535@ad.rit.edu>
2019-02-07 15:46:38 -0500

    Added project

---
+++ ShadowsTest/ShadowsTest/Platform.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace ShadowsTest
+{
+    class Platform
+    {
+        Rectangle rect, shadowRect;
+        Texture2D texture;
+        float lightRot;
+        SpriteBatch sb;
+
+        public bool isInLight;
+
+        public Platform(Rectangle r, Texture2D t, SpriteBatch s)
+        {
+            rect = r;
+            texture = t;
+            sb = s;
+        }
+
+        public void Update(float light)
+        {
+            if (isInLight)
+            {
+                shadowRect = new Rectangle(rect.X + texture.Width / 2, rect.Y + texture.Height / 2, rect.Width * 4, (int)Math.Sqrt(Math.Pow(rect.Width, 2) + Math.Pow(rect.Height, 2)));
+                lightRot = light;
+            }
+        }
+
+        public void Draw()
+        { 
+            if(isInLight)
+            {
+                sb.Draw(texture, destinationRectangle: shadowRect, color: Color.Black, origin: new Vector2(0, rect.Height / 2));
+            }
+            sb.Draw(texture, rect, Color.White);
+        }
+    }
+}
